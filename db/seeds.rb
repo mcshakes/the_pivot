9f2d308228232cb257e56806d4a67271edaf3792
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-01 12:10:08 -0600

    seed data is ready. Note that we only have about 10 for items, users, categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,119 +1,73 @@
+class Seed
 
-class SeedItem
-  def self.create_20_active_items
+  def call
+    generate_items
+    generate_users
+    generate_orders
+  end
+
+  def generate_items
+    generate_categories
+    @item_count = 15
+
     adjectives = ["avant garde", "groundbreaking", "chiaroscuro", "vibrant", "political", "journalistic", "impressionistic", "awe inspiring", "profound", "colorful"]
     photos     = ["mountains", "children at play", "self portrait", "boats", "kittens", "still life", "industrial wasteland", "modernist building", "suburbia"]
-    20.times do
-      Item.create(name: "#{adjectives.sample} #{photos.sample}",
-                  description: "very #{adjectives.sample}",
-                  price: Faker::Commerce.price)
+
+    @item_count.times do
+      item = Item.create(name: "#{adjectives.sample} #{photos.sample}", description: "very #{adjectives.sample}",
+                        price: Faker::Commerce.price, photo: Faker::Avatar.image)
+
+
+        categories = Category.order("RANDOM()").limit(rand(1..5))
+        item.categories << categories
+      puts "Items: #{item.name}"
+    end
+
+  end
+
+  # def generate_vendors
+  #   @vendor_count = 10
+  #
+  #   @vendor_count.times do
+  #     vendor = Vendor.create(name: Faker::Company.name, URL: Faker::Company.name.parameterize, description: Faker::Lorem.sentence)
+  #   end
+  # end
+
+  def generate_users
+    @user_count = 10
+    @user_count.times do
+      user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email,
+      password: Faker::Internet.password, role: 0)
+      # street_address: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state_abbr, zipcode: Faker::Address.zip,
+      # credit_cards: Faker::Business.credit_card_number)
+      puts "users: #{user.first_name} #{user.last_name}"
     end
   end
 
-  def self.retire_5_items
-    5.times do
-      item = Item.all.sample
-      item.status = "retired"
-      item.save!
-    end
-  end
-end
-
-class SeedCategory
-  def self.create_25_categories
-    categories = ["landscape", "portrait", "still life", "architecture", "black and white", "fashion", "documentary", "graffiti"]
-    25.times do
-      Category.create(name: "#{categories.sample}")
-    end
-  end
-end
-
-class SeedItemCategory
-  def self.create_item_category_associations
-    40.times do
-      selected_item = Item.all.sample
-      selected_category = Category.all.sample
-      ItemCategory.create(item_id: selected_item.id,
-                          category_id: selected_category.id)
-    end
-  end
-end
-
-class SeedUser
-  def self.create_rachel
-    User.create(first_name: "Rachel",
-                last_name: "Warbelow",
-                email: "demo+rachel@jumpstartlab.com",
-                password: "password")
-  end
-
-  def self.create_jeff
-    User.create(first_name: "Jeff",
-                last_name: "Casimir",
-                email: "demo+jeff@jumpstartlab.com",
-                password: "password",
-                display_name: "j3")
-  end
-
-  def self.create_jorge
-    User.create(first_name: "Jorge",
-                last_name: "Tellez",
-                email: "demo+jorge@jumpstartlab.com",
-                password: "password",
-                display_name: "novohispano")
-  end
-
-  def self.create_admin_josh
-    User.create(first_name: "Josh",
-                last_name: "Cheek",
-                email: "demo+josh@jumpstartlab.com",
-                password: "password",
-                display_name: "josh",
-                role: "admin")
-  end
-end
-
-class SeedOrder
-  def self.create_10_orders
+  def generate_orders
     10.times do
-      selected_user = User.all.sample
-      order = Order.create(user_id: selected_user.id,
-                           status: "ordered",
-                           created_at: Faker::Time.between(2.days.ago, Time.now),
-                           updated_at: Faker::Time.between(2.days.ago, Time.now))
-      2.times { order.items << Item.all.sample }
+      user  = User.order("RANDOM()").limit(1).first
+      user.orders.create(status: "ordered") do |order|
+        rand(1..10).times do
+          order.items << Item.order("RANDOM()").limit(1).first
+        end
+      end
+      puts "Created Orders For: #{user.first_name}"
     end
   end
 
-  def self.three_completed_orders
-    3.times do
-      order = Order.all.sample
-      order.status = "completed"
-      order.save!
+  def generate_categories
+    @category_count = 10
+
+    @category_count.times do
+      category = Category.create(name: Faker::Commerce.department)
     end
   end
 
-  def self.two_cancelled_orders
-    2.times do
-      order = Order.all.sample
-      order.status = "cancelled"
-      order.save!
-    end
+  def self.call
+    new.call
   end
+
 end
 
-# =========================== Seed Data ===========================
-
-SeedItem.create_20_active_items
-SeedItem.retire_5_items
-SeedCategory.create_25_categories
-SeedItemCategory.create_item_category_associations
-
-SeedUser.create_rachel
-SeedUser.create_jeff
-SeedUser.create_jorge
-SeedUser.create_admin_josh
-
-SeedOrder.create_10_orders
-SeedOrder.three_completed_orders
-SeedOrder.two_cancelled_orders
+Seed.call
