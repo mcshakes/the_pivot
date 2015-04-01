2d12b66118b110f808023e3acf11a356ac6a78ef
Lori Culberson <bigskybandit@sbcglobal.net>
2015-04-01 15:25:30 -0600

    Create index and show view for vendor items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,53 +1,60 @@
 class ItemsController < ApplicationController
+  
+  def index
+    @items = vendor.items
+  end
+
   def show
-    @item = Item.find_by(parameterized_name: params[:item_name])
+    @item = Item.find_by(params[:id])
   end
 
-  def new
-    @item = Item.new
-    authorize! :new, @item
-  end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      update_categories
-      flash[:success] = "New item has been created!"
-      redirect_to menu_item_path(@item)
-    else
-      flash[:error] = "Attributes missing"
-      redirect_to new_menu_item_path
-    end
-  end
 
-  def edit
-    @item = Item.find_by(parameterized_name: params[:item_name])
-    authorize! :edit, @item
-  end
+  # def new
+  #   @item = Item.new
+  #   authorize! :new, @item
+  # end
 
-  def update
-    @item = Item.find_by(parameterized_name: params[:item_name])
-    if @item.update(item_params)
-      update_categories
-      flash[:success] = "Item has been successfully updated!"
-      redirect_to menu_item_path(@item)
-    else
-      flash[:error] = "Attributes missing."
-      redirect_to :back
-    end
-  end
+  # def create
+  #   @item = Item.new(item_params)
+  #   if @item.save
+  #     update_categories
+  #     flash[:success] = "New item has been created!"
+  #     redirect_to menu_item_path(@item)
+  #   else
+  #     flash[:error] = "Attributes missing"
+  #     redirect_to new_menu_item_path
+  #   end
+  # end
 
-  private
+  # def edit
+  #   @item = Item.find_by(parameterized_name: params[:item_name])
+  #   authorize! :edit, @item
+  # end
 
-  def update_categories
-    ItemCategory.destroy_all(item_id: @item.id)
-    params[:category_ids].each do |category|
-      category_id = category.to_i
-      ItemCategory.create(item_id: @item.id, category_id: category_id)
-    end
-  end
+  # def update
+  #   @item = Item.find_by(parameterized_name: params[:item_name])
+  #   if @item.update(item_params)
+  #     update_categories
+  #     flash[:success] = "Item has been successfully updated!"
+  #     redirect_to menu_item_path(@item)
+  #   else
+  #     flash[:error] = "Attributes missing."
+  #     redirect_to :back
+  #   end
+  # end
 
-  def item_params
-    params.require(:item).permit(:name, :description, :price, :image, :category_ids, :status)
-  end
+  # private
+
+  # def update_categories
+  #   ItemCategory.destroy_all(item_id: @item.id)
+  #   params[:category_ids].each do |category|
+  #     category_id = category.to_i
+  #     ItemCategory.create(item_id: @item.id, category_id: category_id)
+  #   end
+  # end
+
+  # def item_params
+  #   params.require(:item).permit(:name, :description, :price, :image, :category_ids, :status)
+  # end
 end
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,4 +2,8 @@ class VendorsController < ApplicationController
   def index
 
   end
+
+  def show
+
+  end
 end
