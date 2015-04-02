663a957785f4d11d6532386c0e8332bcb6ac9f12
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-02 14:26:42 -0600

    made the form and css for the sign up form. havent actually seen it. dream driven development baby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   before_filter :authorize
 
+  def new
+    @user = User.new
+  end
+
   def show
     @orders = Order.where(user_id: current_user.id)
   end
 
+  
   private
 
   def authorize
