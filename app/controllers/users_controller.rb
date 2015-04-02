53b7e58748ef061c34c2a8d2974afc44c21cb831
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-02 14:31:00 -0600

    remade the form to reflect the fact that we ask user to fill in first and last name. Also made some controller changes to the create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,15 @@ class UsersController < ApplicationController
     @orders = Order.where(user_id: current_user.id)
   end
 
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      render :new
+    end
+  end
+
   private
 
   def authorize
@@ -17,4 +25,9 @@ class UsersController < ApplicationController
       redirect_to login_path, alert: "You must sign in"
     end
   end
+
+  def user_params
+    params.require(:user).permit
+  end
+
 end
