0459aa9c28ecef04b21b7b9c18c1b51dbdd539b6
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-04 11:17:08 -0600

    unskipped the two previously failing tests. working now. three tests are skipped but I didn't touch those. assuming someone else skipped so i will leave them.

--- spec/features/unauth_users/user_visits_vendor_index_page_spec.rb
+++ spec/features/unauth_users/user_visits_vendor_index_page_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "user visits vendor index page", type: :feature do
 
-  xit "can see vendors" do
+  it "can see vendors" do
     vendor = FactoryGirl.create(:vendor)
     item = FactoryGirl.create(:item, name: "Long Journey", vendor_id: vendor.id)
-    
+
     visit vendors_path
     within('.container') do
       expect(page).to have_content("Ansel Adams")
--- spec/features/unauth_users/user_visits_vendor_items_page_spec.rb
+++ spec/features/unauth_users/user_visits_vendor_items_page_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "user visits vendor items", type: :feature do
 
-  xit "can see item details" do
+  it "can see item details" do
     vendor = FactoryGirl.create(:vendor)
     item = FactoryGirl.create(:item, name: "Long Journey", vendor_id: vendor.id)
     visit vendor_items_path(slug: vendor.slug)
-    within('.main-vendor-items') do 
+    within('.main-vendor-items') do
       expect(page).to have_content("Long Journey")
     end
   end
