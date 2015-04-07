00f906f9eb7c2d26c0403b17bf64f43e4d0f6cd3
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-07 17:26:10 -0600

    user gets to account just fine. base test for functionality

--- spec/features/auth_users/user_account_spec.rb
+++ spec/features/auth_users/user_account_spec.rb
@@ -2,7 +2,37 @@ require "rails_helper"
 
 RSpec.describe "authenticated user manages personal account", type: :feature do
 
-  it ""
+  # let!(:user) { create(:user)}
+  # before(:each) { set_current_user(user)}
+
+  def user_sign_in(user)
+    visit root_path
+    within(".navbar-nav") {click_link_or_button "Sign In"}
+    within("#signin") do
+      fill_in "session[email]", with: user.email
+      fill_in "session[password]", with: user.password
+      within("form") { click_link_or_button "Sign in" }
+    end
+  end
+
+  it "clicks to go to account page in the nav bar" do
+    user = create(:user, first_name: "Bob", last_name: "Bobbyson", email: "bobson@hotmail.com", password: "password")
+    order = create(:order)
+    item = create(:item)
+    item2 = create(:item, :for_sale, name: "Food Glorious Food")
+    order.items << item
+    user.orders << order
+
+    user_sign_in(user)
+    expect(page).to have_content("Sign in successful")
+    click_link_or_button("Account")
+    expect(current_path).to eq(account_path)
+    expect(page).to have_content("My Account")
+    save_and_open_page
+
+    # save_and_open_page
+    # click_link_or_button("Sign In")
+  end
 end
 
 #
