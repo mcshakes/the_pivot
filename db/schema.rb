597c60676871675a63e4b2303af84605d50c4e92
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-08 12:38:51 -0600

    added table for addresses and fixed rest of tests. Only 3 are failing now.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408175614) do
+ActiveRecord::Schema.define(version: 20150408181438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "addresses", force: :cascade do |t|
+    t.text     "type"
+    t.text     "street"
+    t.text     "city"
+    t.text     "state"
+    t.text     "integer"
+    t.text     "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.text     "name"
