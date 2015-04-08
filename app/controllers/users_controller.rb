25ff0972b05a4479882c4a1b89782a409abbdf20
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-08 12:44:05 -0600

    tests are all passing. Time to write more tests for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :display_name, :email, :password, :password_confirmation, :shipping_address, :billing_address, :credit_card)
+    params.require(:user).permit(:first_name, :last_name, :display_name, :email, :password, :password_confirmation)
   end
 
   def sign_in
