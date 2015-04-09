5cb2e469fd36105d6ca6ac2e00565933276c1a30
Lori Culberson <bigskybandit@sbcglobal.net>
2015-04-09 09:44:21 -0600

    WIP User can click favorite button and post request is sent but hits wrong controller action

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     get  "/account"   =>  "users#show"
 
   resources :users
+  resources :favorite_items
 
   resources :cart_items, only: [:create, :update, :destroy]
   get "/cart", to: "cart_items#index"
--- spec/features/auth_users/user_favorites_spec.rb
+++ spec/features/auth_users/user_favorites_spec.rb
@@ -3,10 +3,19 @@ require "rails_helper"
 RSpec.feature "authenticated user adds favorites", type: :feature do
   attr_reader :vendor, :item
 
+  before do
+      user = create(:user)
+      visit '/'
+      click_on('Sign in')
+      click_on('Sign in')
+      fill_in("session_email", with: user.email)
+      fill_in("session_password", with: user.password)
+      click_button "Sign in"
+  end
+
   def create_vendor_item_user
     @vendor = create(:vendor, name: "Sports Pics")
     @item = create(:item, :for_sale, name: "Super Sold Photograph", vendor: vendor)
-    user = create(:user)
   end
 
   def visit_vendor_click_item
@@ -17,17 +26,19 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
   it "can see a favorites link on the item modal" do
     create_vendor_item_user
     visit_vendor_click_item
+
     expect(page).to have_link("Favorite")
   end
 
-  it "can add favorites to the user dashboard" do
+  xit "can add favorites to the user favorites page" do
     create_vendor_item_user
     visit_vendor_click_item
-    click_link_or_button("Favorite")
-    visit user_path
-    expect(page).to have_link("Super Sold Photograph")
+    click_link("Favorite")
+    click_on('close')
+    click_on ('My Account')
+    click_on('See Favorites')
+    save_and_open_page
+    expect(page).to have_content("Super Sold Photograph")
   end
-
-
 end
 
