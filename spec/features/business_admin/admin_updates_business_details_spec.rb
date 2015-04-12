a9dbe7cdf9222fcecc0870ec7618f4d9ba0f9cfb
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-12 16:24:53 -0600

    added a button to edit page. Failing because no controller action

--- spec/features/business_admin/admin_updates_business_details_spec.rb
+++ spec/features/business_admin/admin_updates_business_details_spec.rb
@@ -9,6 +9,25 @@ RSpec.describe "admin updates business details", type: :feature do
     expect(page).to have_content("#{@vendor.description}")
   end
 
+  xit "won't allow admin of other stores to see the page" do
+    admin1 = create(:admin)
+    vendor1 = create(:vendor)
+    admin1.vendors << vendor1
+    other_admin = User.create(first_name: "Hey", last_name: "There", email: "admin2@example.com",
+                              password: "admin", role: "store_admin")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(other_admin)
+    visit vendor_items_path(slug: vendor1.slug)
+    save_and_open_page
+    expect(page).to have_content("You are not authorized to access this page")
+  end
+
+  it "can update the business name" do
+    create_admin_user_and_vendor
+    visit vendor_items_path(slug: @vendor.slug)
+    click_link_or_button("Edit Business Info")
+
+  end
+
 
   private
 
