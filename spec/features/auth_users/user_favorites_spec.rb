b506f5636cf08b269806dd075c651d15eb87d388
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-13 16:26:01 -0600

    merged master

--- spec/features/auth_users/user_favorites_spec.rb
+++ spec/features/auth_users/user_favorites_spec.rb
@@ -4,13 +4,13 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
   attr_reader :vendor, :item
 
   before do
-      user = create(:user)
-      visit '/'
-      click_on('Sign in')
-      click_on('Sign in')
-      fill_in("session_email", with: user.email)
-      fill_in("session_password", with: user.password)
-      click_button "Sign in"
+    user = create(:user)
+    visit '/'
+    click_on('Sign in')
+    click_on('Sign in')
+    fill_in("session_email", with: user.email)
+    fill_in("session_password", with: user.password)
+    click_button "Sign in"
   end
 
   def create_vendor_item_user
@@ -18,7 +18,7 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
     @item = create(:item, :for_sale, name: "Super Sold Photograph", vendor: vendor)
   end
 
-  def visit_vendor_click_item_favorite_link 
+  def visit_vendor_click_item_favorite_link
     visit vendor_items_path(slug: vendor.slug)
     click_link_or_button(item.name)
     click_link("Favorite")
@@ -26,7 +26,7 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
 
   it "can see a favorites link on the item modal" do
     create_vendor_item_user
-    visit_vendor_click_item_favorite_link 
+    visit_vendor_click_item_favorite_link
     visit vendor_items_path(slug: item.vendor.slug)
     click_link_or_button(item.name)
 
@@ -35,16 +35,17 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
 
   it "can add favorites to the user favorites page" do
     create_vendor_item_user
-    visit_vendor_click_item_favorite_link 
+    visit_vendor_click_item_favorite_link
     visit vendor_items_path(slug: vendor.slug)
     click_on('My Account')
     click_on('See Favorites')
+
     expect(page).to have_content("Super Sold Photograph")
   end
 
   it "can add favorite image only once" do
     create_vendor_item_user
-    visit_vendor_click_item_favorite_link 
+    visit_vendor_click_item_favorite_link
     click_link_or_button(item.name)
     click_link("Favorite")
     visit favorites_path
@@ -54,7 +55,7 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
 
   it "can not see the favorite button when item favorited" do
     create_vendor_item_user
-    visit_vendor_click_item_favorite_link 
+    visit_vendor_click_item_favorite_link
     visit favorites_path
     click_link_or_button(item.name)
     within('#modal') do
@@ -63,4 +64,3 @@ RSpec.feature "authenticated user adds favorites", type: :feature do
   end
 
 end
-
