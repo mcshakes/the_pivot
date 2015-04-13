051b8f00023b5222faea6c6f9852da24d3e9b8c2
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-13 16:29:03 -0600

    test now success. removed .utc and :number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,7 @@ module ApplicationHelper
   def cache_key_for(model)
     prefix = model.to_s.downcase.pluralize
     count = model.count
-    max_updated_at =  model.maximum(:updated_at).utc.to_s(:number)
+    max_updated_at =  model.maximum(:updated_at).to_s
 
     "#{prefix}/all-#{count}-#{max_updated_at}"
   end
