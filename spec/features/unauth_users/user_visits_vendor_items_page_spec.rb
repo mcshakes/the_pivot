8f96590392c294b06f4aaa208a80cc3a069d4220
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-13 21:45:33 -0600

    removed caching to restore functionality

--- spec/features/unauth_users/user_visits_vendor_items_page_spec.rb
+++ spec/features/unauth_users/user_visits_vendor_items_page_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe "user visits vendor items", type: :feature do
     end
   end
 
+  it "sees a different store every time" do
+    vendor = FactoryGirl.create(:vendor)
+    item = FactoryGirl.create(:item, name: "Long Journey", vendor_id: vendor.id)
+    visit vendor_items_path(slug: vendor.slug)
+    within('.main-vendor-items') do
+      expect(page).to have_content("Long Journey")
+    end
+    vendor2 = Vendor.create(name: "New Vendor", description: "cool stuff", credit_card: "1111222233334444", user_id: 2)
+    item2 = FactoryGirl.create(:item, name: "Short Journey", vendor_id: vendor2.id)
+    visit vendor_items_path(slug: vendor2.slug)
+    within('.main-vendor-items') do
+      expect(page).to have_content("Long Journey")
+    end
+  end
+
 end
