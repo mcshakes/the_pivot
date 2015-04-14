03876d4822787179218f2cc2432c1cf21fee6c1a
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-13 19:52:30 -0600

    working on active record queries

--- app/controllers/vendors/items_controller.rb
+++ app/controllers/vendors/items_controller.rb
@@ -3,7 +3,15 @@ class Vendors::ItemsController < ApplicationController
   def index
     @heading = "Welcome to #{current_vendor.name}'s Photo Gallery"
     @items = current_vendor.items
-    @categories = Category.all
+
+    # items = Vendor.first.items.includes(:categories).all
+    # categories = items.flat_map(&:categories).uniq
+
+    # iterate through items
+    # find all categories with item id
+    # make sure categories are unique
+      item_categories = @items.includes(:categories).all
+    @categories = item_categories.flat_map(&:categories).uniq
   end
 
   def show
