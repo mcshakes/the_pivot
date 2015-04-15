c47dd96d63d76eceb8f8ef94938fc177b6a8fd38
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-15 17:18:16 -0600

    Removed unneeded order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,9 @@
 class Order < ActiveRecord::Base
-  include CacheInvalidator
 
-  validates :user_id, :status, presence: true
+  validates :user_id, presence: true
   has_many :item_orders
   has_many :items, through: :item_orders
   belongs_to :user
-
-  scope :ordered, -> { where(status: "ordered") }
-  scope :cancelled, -> { where(status: "cancelled") }
-  scope :completed, -> { where(status: "completed") }
 
   def total_price
     items.inject(0) { |sum, item| sum + item.price }
