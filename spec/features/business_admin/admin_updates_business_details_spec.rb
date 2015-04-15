cb4a607b63784c36dbe3d5a3dee084910d63c00a
Lori Culberson <bigskybandit@sbcglobal.net>
2015-04-14 23:26:21 -0600

    Repair merge conflicts

--- spec/features/business_admin/admin_updates_business_details_spec.rb
+++ spec/features/business_admin/admin_updates_business_details_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "admin updates business details", type: :feature do
   it "sees business info when clicking on individual store" do
     create_admin_user_and_vendor
     visit vendor_items_path(slug: @vendor.slug)
-    expect(page).to have_content("Welcome to #{@vendor.name}'s Photo Gallery")
+    expect(page).to have_content("Welcome to #{@vendor.name}")
     expect(page).to have_content("#{@vendor.description}")
   end
 
