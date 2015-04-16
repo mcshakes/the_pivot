af4e9cf2bde618fa40a1ddb16024ae9557f4f4fe
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-16 09:19:31 -0600

    corrected user invalid inputs spec, and deleted the custom errors for matching password. It's because we aren't using errors with full messages

--- spec/features/unauth_users/user_creates_a_profile_spec.rb
+++ spec/features/unauth_users/user_creates_a_profile_spec.rb
@@ -38,21 +38,12 @@ RSpec.feature "Unauthenticated user", type: :feature do
     expect(page).to have_content("Last name")
   end
 
-  xit "gets error messages if form is filled with invalid inputs" do
-    get_registration_form
-    incorrect_registration
-    click_link_or_button("Create Account")
-
-    expect(page).to have_content("The form contains 2 errors")
-    expect(page).to have_content("Email is invalid")
-    expect(page).to have_content("Password can't be blank")
-  end
-
   it "gets error messages if form is filled with invalid inputs" do
     get_registration_form
     incorrect_registration
     click_link_or_button("Create Account")
     expect(page).to have_content("Invalid input, please try again")
+    expect(current_path).to eq(root_path)
   end
 
   it "is taken back to whatever page they were on upon correct registration" do
@@ -60,19 +51,6 @@ RSpec.feature "Unauthenticated user", type: :feature do
     correct_registration
     click_link_or_button("Create Account")
     expect(current_path).to eq(root_path)
-  end
-
-  xit "must have a matching password confirmation" do
-    get_registration_form
-    fill_in("user[first_name]", with: "Pip" )
-    fill_in("user[last_name]", with: "Pippo" )
-    fill_in("user[display_name]", with: "Pip" )
-    fill_in("user[email]", with: "pipmagnet@yahoo.com" )
-    fill_in("user[password]", with: "password" )
-    click_link_or_button("Create Account")
-
-    expect(page).to have_content("The form contains 1 error")
-    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 
   it "receives an email confirmation upon sign up" do
